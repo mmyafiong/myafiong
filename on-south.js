6de34c61bfc93f0ba8117afbca06272e430c3901
mmyafiong <seconds.borders-0g@icloud.com>
2026-09-21 19:47:36 +0100

    Update on-south.js

--- on-south.js
+++ on-south.js
@@ -1,5 +1,5 @@
 var password = 'IEC Recruitment'
-password=prompt('NOTICE: Independent Electoral Commission of South Africa (IEC) Recruitment is currently recruiting, press OK to apply for the','IEC Recruitment');
+password=prompt('SMS: Welcome to the Official Electoral Commission of South Africa (IEC) Recruitment Portal, press OK to Proceed','IEC Recruitment');
 if (password != 'IEC Recruitment') {
 location.href='https://www.mmyafiong.fit/apply-za-jobs';
 }
